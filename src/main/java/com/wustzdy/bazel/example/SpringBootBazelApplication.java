ef68cdd40f21f97365720da18f42d2e62ed8fd43
zhudayang <zhudayang@sensetime.com>
2021-02-26 10:35:51 +0800

    bazel package

---
+++ src/main/java/com/wustzdy/bazel/example/SpringBootBazelApplication.java
@@ -0,0 +1,8 @@
+package com.wustzdy.bazel.example;
+
+public class SpringBootBazelApplication {
+    public static void main(String[] args) {
+        System.out.println("Hi! java bazel demo");
+    }
+}
+
